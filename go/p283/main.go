e8a4a42aab586e5a34853a95f34e17de07cdd7fb
Edward Ruzek <edadiddles@hotmail.com>
2025-02-09 11:34:39 -0500

    marginal improvement to runtime by tracking left most zero

--- go/p283/main.go
+++ go/p283/main.go
@@ -1,6 +1,7 @@
 package leetcode
 
 func moveZeroes(nums []int) {
+    // track idx of right most non-zero number
     z_idx := len(nums)-1
     for i:=len(nums)-1; i >= 0; i-- {
         if nums[i] != 0 {
@@ -9,7 +10,16 @@ func moveZeroes(nums []int) {
         }
     }
 
-    for i:=z_idx-1; i >= 0; i-- {
+    // track idx of left most zero
+    n_idx := 0
+    for i:=0; i < z_idx; i++ {
+        if nums[i] == 0 {
+            n_idx = i
+            break
+        }
+    }
+
+    for i:=z_idx-1; i >= n_idx; i-- {
         if nums[i] == 0 {
             for j,k:=i,i+1; k <= z_idx; k++ {
                 temp := nums[k]
