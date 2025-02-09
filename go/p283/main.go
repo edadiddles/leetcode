694d8233e1282eb86c309738062677d585ea6b49
Edward Ruzek <edadiddles@hotmail.com>
2025-02-09 13:26:51 -0500

    updating to use originally thought solution before seeing it had to be updated in place. And modifying to run through array copying values to original array

--- go/p283/main.go
+++ go/p283/main.go
@@ -1,33 +1,15 @@
 package leetcode
 
 func moveZeroes(nums []int) {
-    // track idx of right most non-zero number
-    z_idx := len(nums)-1
-    for i:=len(nums)-1; i >= 0; i-- {
+    temp := make([]int, len(nums))
+    for i,j:= 0,0; i < len(nums); i++ {
         if nums[i] != 0 {
-            z_idx = i
-            break
+            temp[j] = nums[i]
+            j++
         }
     }
 
-    // track idx of left most zero
-    n_idx := 0
-    for i:=0; i < z_idx; i++ {
-        if nums[i] == 0 {
-            n_idx = i
-            break
-        }
-    }
-
-    for i:=z_idx-1; i >= n_idx; i-- {
-        if nums[i] == 0 {
-            for j,k:=i,i+1; k <= z_idx; k++ {
-                temp := nums[k]
-                nums[k] = nums[j]
-                nums[j] = temp
-                j++
-            }
-            z_idx--
-        }
+    for i:=0; i < len(nums); i++ {
+        nums[i] = temp[i]
     }
 }
