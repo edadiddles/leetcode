2c94b8ec5e2ee67f133d6ba1cdfc6e950ca528e9
Edward Ruzek <edadiddles@hotmail.com>
2025-10-10 09:20:09 -0400

    updating solution to use overwrite matrix val with special data for removing the duplicate value bug

--- python/p54/main.py
+++ python/p54/main.py
@@ -8,15 +8,16 @@ class Solution:
         spiral_arr = []
         while True:
             spiral_arr.append(matrix[pos[1]][pos[0]])
+            matrix[pos[1]][pos[0]] = -999  # constraint is matrix vals are in the range [-100,100]
 
             x = pos[0]+dir[0]
             y = pos[1]+dir[1]
 
-            if y >= len(matrix) or x >= len(matrix[0]) or matrix[y][x] in spiral_arr:
+            if y >= len(matrix) or x >= len(matrix[0]) or matrix[y][x] == -999:
                 dir = [-dir[1], dir[0]]
                 x = pos[0]+dir[0]
                 y = pos[1]+dir[1]
-                if y >= len(matrix) or x >= len(matrix[0]) or matrix[y][x] in spiral_arr:
+                if y >= len(matrix) or x >= len(matrix[0]) or matrix[y][x] == -999:
                     return spiral_arr
 
             pos = [x, y]
