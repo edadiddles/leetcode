239c7290adcbdd134cc6f5de511097a48bb473c9
Edward Ruzek <edadiddles@hotmail.com>
2025-10-12 14:45:42 -0400

    best solution

--- python/p1523/main.py
+++ python/p1523/main.py
@@ -1,9 +1,3 @@
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
-        num_odds = (high - low) // 2
-        if low % 2 == 1:
-            num_odds += 1
-        elif high % 2 == 1:
-            num_odds += 1
-
-        return num_odds
+        return (high+1)//2 - low//2
