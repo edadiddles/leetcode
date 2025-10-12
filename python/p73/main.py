9c68e2828c62cf4cfb4bfa7548c94f1db5e95272
Edward Ruzek <edadiddles@hotmail.com>
2025-10-12 14:15:25 -0400

    removed function call and saved some memory and improved runtime.

--- python/p73/main.py
+++ python/p73/main.py
@@ -9,36 +9,37 @@ class Solution:
         i, j = 0, 0
         while i < len(matrix):
             while j < len(matrix[0]):
-                self.checkNode(matrix, i, j)
+                if i > 0 and matrix[i-1][j] is None:
+                    matrix[i-1][j] = 0
+
+                if len(matrix) == i+1 and matrix[i][j] is None:
+                    matrix[i][j] = 0
+                    j += 1
+                    continue
+                if matrix[i][j] != 0:
+                    j += 1
+                    continue
+
+                for k in range(len(matrix)):
+                    if matrix[k][j] == 0:
+                        continue
+                    elif k < i:
+                        matrix[k][j] = 0
+                        continue
+
+                    matrix[k][j] = None
+
+                for k in range(len(matrix[i])):
+                    if matrix[i][k] == 0:
+                        continue
+                    elif k < j:
+                        matrix[i][k] = 0
+                        continue
+
+                    matrix[i][k] = None
                 j += 1
 
             j = 0
             i += 1
 
-    def checkNode(self, matrix: List[List[int]], i: int, j: int) -> None:
-        if i > 0 and matrix[i-1][j] is None:
-            matrix[i-1][j] = 0
 
-        if len(matrix) == i+1 and matrix[i][j] is None:
-            matrix[i][j] = 0
-            return
-        if matrix[i][j] != 0:
-            return
-
-        for k in range(len(matrix)):
-            if matrix[k][j] == 0:
-                continue
-            elif k < i:
-                matrix[k][j] = 0
-                continue
-
-            matrix[k][j] = None
-
-        for k in range(len(matrix[i])):
-            if matrix[i][k] == 0:
-                continue
-            elif k < j:
-                matrix[i][k] = 0
-                continue
-
-            matrix[i][k] = None
