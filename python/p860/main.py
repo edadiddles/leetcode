ccfcc42ff6f519e9f15a113d7a7749b190accd57
Edward Ruzek <edadiddles@hotmail.com>
2025-10-13 14:08:01 -0400

    updating soln attempt to track bills in till and verify the till contains the correct bills when giving change

--- python/p860/main.py
+++ python/p860/main.py
@@ -3,12 +3,18 @@ from typing import List
 
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        till = 0
+        till = {"5": 0, "10": 0, "20": 0}
         for bill in bills:
-            till -= (bill-5)
-            if till < 0:
-                return False
-
-            till += 5
+            till[str(bill)] += 1
+            bill -= 5
+            while bill > 0:
+                if bill >= 10 and till["10"] > 0:
+                    till["10"] -= 1
+                    bill -= 10
+                elif till["5"] > 0:
+                    till["5"] -= 1
+                    bill -= 5
+                else:
+                    return False
 
         return True
