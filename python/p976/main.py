ee4a3256553a526ffabb07ad4bfea5fed824b96a
Edward Ruzek <edadiddles@hotmail.com>
2025-10-18 11:15:17 -0400

    reversed loop order to check for max at the end of array to avoid having to find every perimeter and can break when finding first max perimeter

--- python/p976/main.py
+++ python/p976/main.py
@@ -5,18 +5,22 @@ class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
         nums = sorted(nums)
 
-        max_perimeter = 0
-        for i in range(0, len(nums)-2):
-            for j in range(i+1, len(nums)-1):
-                for k in range(j+1, len(nums)):
+        i = len(nums) - 3
+        while i >= 0:
+            j = len(nums) - 2
+            while j >= 1 and j > i:
+                k = len(nums) - 1
+                while k >= 2 and k > j:
                     a = nums[i]
                     b = nums[j]
                     c = nums[k]
 
                     perimeter = a+b+c
-                    if c < a or c >= a+b:
-                        break
-                    elif max_perimeter < perimeter:
-                        max_perimeter = perimeter
+                    if c < a+b:
+                        return perimeter
 
-        return max_perimeter
+                    k -= 1
+                j -= 1
+            i -= 1
+
+        return 0
