c26670a852ec70eb84877e1d9acbd633ef67081a
Edward Ruzek <edadiddles@hotmail.com>
2025-10-30 09:33:05 -0400

    initial attempt at walking graph. kinda convoluted

---
+++ python/p802/main.py
@@ -0,0 +1,40 @@
+from typing import List
+
+
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        safe_nodes = [1]*len(graph)
+
+        for i in range(len(safe_nodes)):
+            if safe_nodes[i] == 0:
+                print(f"node not safe: ({i})")
+                continue
+
+            visited_nodes = [0]*len(graph)
+            print(f"starting walk: {i}")
+            if not self.r_walk(graph, safe_nodes, visited_nodes, i):
+                print(f"walk not safe: ({i})")
+                safe_nodes[i] = 0
+
+        return [idx for idx, val in enumerate(safe_nodes) if val == 1]
+
+
+    def r_walk(self, graph: List[List[int]], safe_nodes: List[int], visited_nodes: List[int], node_idx: int):
+        visited_nodes[node_idx] = 1
+        for i in range(len(graph[node_idx])):
+            if graph[node_idx][i] == node_idx:
+                print(f"graph[i]==node_idx: ({node_idx}, {i})")
+                safe_nodes[node_idx] = 0
+                return False
+            elif safe_nodes[graph[node_idx][i]] == 0:
+                print(f"safe_nodes[i] == 0: ({node_idx},{i})")
+                safe_nodes[node_idx] = 0
+                return False
+            elif visited_nodes[graph[node_idx][i]] == 1:
+                print(f"visited_nodes[i] == 1: ({node_idx},{i})")
+                safe_nodes[i] = 0
+                return False
+
+            self.r_walk(graph, safe_nodes, visited_nodes, graph[node_idx][i])
+
+        return True
