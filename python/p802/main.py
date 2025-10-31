3ddf34945fafc9d9febbef02d0391623ee8db65d
Edward Ruzek <edadiddles@hotmail.com>
2025-10-30 22:24:40 -0400

    fixed bug that set the wrong node. but I think that was thought to be correct at one point

--- python/p802/main.py
+++ python/p802/main.py
@@ -6,39 +6,35 @@ class Solution:
         safe_nodes = [1]*len(graph)
 
         for i in range(len(safe_nodes)):
+            print(safe_nodes)
             if safe_nodes[i] == 0:
                 print(f"node not safe: ({i})")
                 continue
 
             for j in range(len(graph[i])):
                 visited_nodes = [0]*len(graph)
-                print(f"starting walk: {i},{j}")
                 if not self.r_walk(graph, safe_nodes, visited_nodes, graph[i][j]):
-                    print(f"walk not safe: ({i})")
                     safe_nodes[i] = 0
-                else:
-                    print(f"walk safe")
+                    break
 
         return [idx for idx, val in enumerate(safe_nodes) if val == 1]
 
 
     def r_walk(self, graph: List[List[int]], safe_nodes: List[int], visited_nodes: List[int], node_idx: int):
+        print(f"visiting node: {node_idx}")
         visited_nodes[node_idx] = 1
         for i in range(len(graph[node_idx])):
             visited_nodes_cpy = visited_nodes.copy()
             if graph[node_idx][i] == node_idx:
-                print(f"graph[i]==node_idx: ({node_idx}, {i})")
                 safe_nodes[node_idx] = 0
                 print(f"node not safe: {node_idx}")
                 return False
             elif safe_nodes[graph[node_idx][i]] == 0:
-                print(f"safe_nodes[i] == 0: ({node_idx},{i})")
                 safe_nodes[node_idx] = 0
                 print(f"node not safe: {node_idx}")
                 return False
             elif visited_nodes_cpy[graph[node_idx][i]] == 1:
-                print(f"visited_nodes[i] == 1: ({node_idx},{i})")
-                safe_nodes[i] = 0
+                safe_nodes[node_idx] = 0
                 print(f"node not safe: {node_idx}")
                 return False
 
--- python/p802/test_main.py
+++ python/p802/test_main.py
@@ -24,3 +24,8 @@ def test4():
 def test5():
     soln = Solution()
     assert soln.eventualSafeNodes([[1,2,4], [0,2,4], [3,4], [4], [2]]) == []
+
+
+def test6():
+    soln = Solution()
+    assert soln.eventualSafeNodes([[0,6,7,9], [], [], [], [2,6,8], [7,9], [7,8,9], [], [6,9], [7]]) == [1,2,3,5,7,9]
