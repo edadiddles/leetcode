43c5f04b2a5e25dfbbb18b1a6c83612675c909ac
Edward Ruzek <edadiddles@hotmail.com>
2025-10-30 23:24:46 -0400

    tracking visited nodes for safe nodes and unsafe nodes when finishing walk and includes fast exit when known safe

--- python/p802/main.py
+++ python/p802/main.py
@@ -3,39 +3,50 @@ from typing import List
 
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        safe_nodes = [1]*len(graph)
+        safe_nodes = [-1]*len(graph)
 
         for i in range(len(safe_nodes)):
-            print(safe_nodes)
             if safe_nodes[i] == 0:
-                print(f"node not safe: ({i})")
                 continue
 
+            visited_nodes_master = [0]*len(graph)
             for j in range(len(graph[i])):
                 visited_nodes = [0]*len(graph)
                 if not self.r_walk(graph, safe_nodes, visited_nodes, graph[i][j]):
+                    print(visited_nodes)
                     safe_nodes[i] = 0
+                    for idx in range(len(visited_nodes)):
+                        if visited_nodes[idx] == 1:
+                            if idx in (2,5,7,9):
+                                print(f"setting {idx} as unsafe for ({i},{j})")
+                            safe_nodes[idx] = 0
                     break
+                else:
+                    for idx in range(len(visited_nodes)):
+                        if visited_nodes[idx] == 1:
+                            visited_nodes_master[idx] = 1
 
-        return [idx for idx, val in enumerate(safe_nodes) if val == 1]
+            if safe_nodes[i] == -1:
+                safe_nodes[i] = 1
+                for idx in range(len(visited_nodes_master)):
+                    if visited_nodes_master[idx] == 1:
+                        safe_nodes[idx] = 1
+
+        return [idx for idx, val in enumerate(safe_nodes) if val == 1 or val == -1]
 
 
     def r_walk(self, graph: List[List[int]], safe_nodes: List[int], visited_nodes: List[int], node_idx: int):
-        print(f"visiting node: {node_idx}")
         visited_nodes[node_idx] = 1
+        if safe_nodes[node_idx] == 1:
+            return True
+
         for i in range(len(graph[node_idx])):
             visited_nodes_cpy = visited_nodes.copy()
             if graph[node_idx][i] == node_idx:
-                safe_nodes[node_idx] = 0
-                print(f"node not safe: {node_idx}")
                 return False
             elif safe_nodes[graph[node_idx][i]] == 0:
-                safe_nodes[node_idx] = 0
-                print(f"node not safe: {node_idx}")
                 return False
             elif visited_nodes_cpy[graph[node_idx][i]] == 1:
-                safe_nodes[node_idx] = 0
-                print(f"node not safe: {node_idx}")
                 return False
 
             if not self.r_walk(graph, safe_nodes, visited_nodes_cpy, graph[node_idx][i]):
