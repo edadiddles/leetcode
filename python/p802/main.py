bab7b420551d9a969bc4aa944b0945159c1eb1ce
Edward Ruzek <edadiddles@hotmail.com>
2025-10-31 20:57:47 -0400

    rewriting solution after reading leetcode editorial. kept dfs approach but changed my logic to assume node unsafe until it is determined to safe and undoing the flag as post condition.

--- python/p802/main.py
+++ python/p802/main.py
@@ -3,66 +3,26 @@ from typing import List
 
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        safe_nodes = [-1]*len(graph)
+        unsafe_nodes = [False]*len(graph)
+        visited_nodes = [False]*len(graph)
 
-        for i in range(len(safe_nodes)):
-            if safe_nodes[i] == 0:
-                continue
-            elif safe_nodes[i] == 1:
-                continue
+        for i in range(len(graph)):
+            self.r_walk(graph, unsafe_nodes, visited_nodes, i)
 
-            visited_nodes_master = [0]*len(graph)
-            for j in range(len(graph[i])):
-                visited_nodes = [0]*len(graph)
-                if not self.r_walk(graph, safe_nodes, visited_nodes, graph[i][j]):
-                    safe_nodes[i] = 0
-                    break
-                else:
-                    for idx in range(len(visited_nodes)):
-                        if visited_nodes[idx] == 1:
-                            visited_nodes_master[idx] = 1
-
-            if safe_nodes[i] == -1:
-                safe_nodes[i] = 1
-                for idx in range(len(visited_nodes_master)):
-                    if visited_nodes_master[idx] == 1:
-                        safe_nodes[idx] = 1
-
-        return [idx for idx, val in enumerate(safe_nodes) if val == 1]
+        return [idx for idx, val in enumerate(unsafe_nodes) if not val]
 
 
-    def r_walk(self, graph: List[List[int]], safe_nodes: List[int], visited_nodes: List[int], node_idx: int):
-        visited_nodes[node_idx] = 1
-        if safe_nodes[node_idx] == 1:
-            for idx in range(len(visited_nodes)):
-                if visited_nodes[idx] == 1:
-                    safe_nodes[idx] = 1
+    def r_walk(self, adj_list: List[List[int]], unsafe_nodes: List[int], visited_nodes: List[int], node_idx: int):
+        if unsafe_nodes[node_idx]:
             return True
-        elif safe_nodes[node_idx] == 0:
-            for idx in range(len(visited_nodes)):
-                if visited_nodes[idx] == 1:
-                    safe_nodes[idx] = 0
+        elif visited_nodes[node_idx]:
             return False
 
-        for i in range(len(graph[node_idx])):
-            visited_nodes_cpy = visited_nodes.copy()
-            if graph[node_idx][i] == node_idx:
-                for idx in range(len(visited_nodes)):
-                    if visited_nodes[idx] == 1:
-                        safe_nodes[idx] = 0
-                return False
-            elif safe_nodes[graph[node_idx][i]] == 0:
-                for idx in range(len(visited_nodes)):
-                    if visited_nodes[idx] == 1:
-                        safe_nodes[idx] = 0
-                return False
-            elif visited_nodes_cpy[graph[node_idx][i]] == 1:
-                for idx in range(len(visited_nodes)):
-                    if visited_nodes[idx] == 1:
-                        safe_nodes[idx] = 0
-                return False
+        visited_nodes[node_idx] = True
+        unsafe_nodes[node_idx] = True
+        for n in adj_list[node_idx]:
+            if self.r_walk(adj_list, unsafe_nodes, visited_nodes, n):
+                return True
 
-            if not self.r_walk(graph, safe_nodes, visited_nodes_cpy, graph[node_idx][i]):
-                return False
-
-        return True
+        unsafe_nodes[node_idx] = False
+        return False
