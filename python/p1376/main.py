f5359c468c834943eb9f2e789344b17ff779ff68
Edward Ruzek <edadiddles@hotmail.com>
2025-11-02 18:34:10 -0500

    removing the jank

--- python/p1376/main.py
+++ python/p1376/main.py
@@ -5,8 +5,6 @@ class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
         manager_map = {}
         for i in range(n):
-            if i not in manager_map:
-                manager_map[i] = []
             if manager[i] == -1:
                 continue
             if manager[i] not in manager_map:
@@ -19,7 +17,7 @@ class Solution:
 
 
     def r_walk(self, node: int, time: int, manager_map, informTime: List[int]):
-        subordinates = manager_map[node]
+        subordinates = manager_map[node] if node in manager_map else []
         for n in range(len(subordinates)):
             informTime[subordinates[n]] += time
             self.r_walk(subordinates[n], informTime[subordinates[n]], manager_map, informTime)
