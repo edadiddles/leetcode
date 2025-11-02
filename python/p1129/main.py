bc7233546c389eaab36fcf5068d5e31bb8df4d1a
Edward Ruzek <edadiddles@hotmail.com>
2025-11-01 20:48:21 -0400

    got a working solution. probably a memory hog

--- python/p1129/main.py
+++ python/p1129/main.py
@@ -4,18 +4,36 @@ from typing import List
 class Solution:
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
         shortest_paths = [-1]*n
+        redEdges.sort(key=lambda x: x[0])
+        blueEdges.sort(key=lambda x: x[0])
 
-        self.r_walk(0, 0, shortest_paths, redEdges, blueEdges)
-        self.r_walk(0, 0, shortest_paths, blueEdges, redEdges)
+        self.r_walk([0], 0, shortest_paths, [False]*n, [False]*n, redEdges, blueEdges)
+        self.r_walk([0], 0, shortest_paths, [False]*n, [False]*n, blueEdges, redEdges)
         return shortest_paths
 
 
-    def r_walk(self, n: int, depth: int, shortest_paths: List[int], primary_edges: List[List[int]], secondary_edges: List[List[int]]):
-        if shortest_paths[n] == -1 or depth < shortest_paths[n]:
-            shortest_paths[n] = depth
+    def r_walk(self, nodes: List[int], depth: int, shortest_paths: List[int], visited_primary: List[int], visited_secondary: List[int], primary_edges: List[List[int]], secondary_edges: List[List[int]]):
+        if len(nodes) == 0:
+            return
 
-        for i in range(len(primary_edges)):
-            if primary_edges[i][0] != n:
+        next_edges = primary_edges if depth%2 == 0 else secondary_edges
+        visited = visited_primary if depth%2 == 0 else visited_secondary
+
+        q = []
+        for node in nodes:
+            if visited[node]:
                 continue
 
-            self.r_walk(primary_edges[i][1], depth+1, shortest_paths, secondary_edges, [edge for k, edge in enumerate(primary_edges) if i != k])
+            visited[node] = True
+            if shortest_paths[node] == -1 or shortest_paths[node] > depth:
+                shortest_paths[node] = depth
+
+            for next_edge in next_edges:
+                if node > next_edge[0]:
+                    continue
+                if node < next_edge[0]:
+                    break
+
+                q.append(next_edge[1])
+
+        self.r_walk(q, depth+1, shortest_paths, visited_primary, visited_secondary, primary_edges, secondary_edges)
