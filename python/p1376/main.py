a53811c44a363fa5a8bad354187c1fc515c851a3
Edward Ruzek <edadiddles@hotmail.com>
2025-11-02 18:32:18 -0500

    removed loop to get subordinates and placed in map which was causing O(n) per recursive walk. doing some jank to prevent key errors

--- python/p1376/main.py
+++ python/p1376/main.py
@@ -3,13 +3,23 @@ from typing import List
 
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
-        self.r_walk(headID, informTime[headID], manager, informTime)
+        manager_map = {}
+        for i in range(n):
+            if i not in manager_map:
+                manager_map[i] = []
+            if manager[i] == -1:
+                continue
+            if manager[i] not in manager_map:
+                manager_map[manager[i]] = []
+
+            manager_map[manager[i]] += [i]
+
+        self.r_walk(headID, informTime[headID], manager_map, informTime)
         return max(informTime)
 
 
-    def r_walk(self, node: int, time: int, manager: List[int], informTime: List[int]):
-        subordinates = [i for i in range(len(manager)) if manager[i] == node]
-
-        for n in subordinates:
-            informTime[n] += time
-            self.r_walk(n, informTime[n], manager, informTime)
+    def r_walk(self, node: int, time: int, manager_map, informTime: List[int]):
+        subordinates = manager_map[node]
+        for n in range(len(subordinates)):
+            informTime[subordinates[n]] += time
+            self.r_walk(subordinates[n], informTime[subordinates[n]], manager_map, informTime)
